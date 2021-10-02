edf358d736ddf36eb84a056d135ca65589228628
Ariel Mashraki <ariel@mashraki.co.il>
2021-10-02 19:17:22 +0300

    initial commit

---
+++ context.go
@@ -0,0 +1,83 @@
+package entcache
+
+import (
+	"context"
+	"time"
+)
+
+type ctxKey struct{}
+
+// NewContext returns a new Context that carries a cache.
+func NewContext(ctx context.Context, levels ...AddGetDeleter) context.Context {
+	var cache AddGetDeleter
+	switch len(levels) {
+	case 0:
+		cache = NewLRU(0)
+	case 1:
+		cache = levels[0]
+	default:
+		cache = &multiLevel{levels: levels}
+	}
+	return context.WithValue(ctx, ctxKey{}, cache)
+}
+
+// FromContext returns the cache value stored in ctx, if any.
+func FromContext(ctx context.Context) (AddGetDeleter, bool) {
+	c, ok := ctx.Value(ctxKey{}).(AddGetDeleter)
+	return c, ok
+}
+
+// ctxOptions allows injecting runtime options.
+type ctxOptions struct {
+	skip  bool          // i.e. skip entry.
+	evict bool          // i.e. skip and invalidate entry.
+	key   Key           // entry key.
+	ttl   time.Duration // entry duration.
+}
+
+var ctxOptionsKey ctxOptions
+
+// Skip returns a new Context that tells the Driver
+// to skip the cache entry on Query.
+func Skip(ctx context.Context) context.Context {
+	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
+	if !ok {
+		return context.WithValue(ctx, ctxOptionsKey, &ctxOptions{skip: true})
+	}
+	c.skip = true
+	return ctx
+}
+
+// Evict returns a new Context that tells the Driver
+// to skip and invalidate the cache entry on Query.
+func Evict(ctx context.Context) context.Context {
+	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
+	if !ok {
+		return context.WithValue(ctx, ctxOptionsKey, &ctxOptions{skip: true, evict: true})
+	}
+	c.skip = true
+	c.evict = true
+	return ctx
+}
+
+// WithKey returns a new Context that carries the Key for the cache entry.
+// Note that, this option should not be used if the ent.Client query involves
+// more than 1 SQL query (e.g. eager loading).
+func WithKey(ctx context.Context, key Key) context.Context {
+	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
+	if !ok {
+		return context.WithValue(ctx, ctxOptionsKey, &ctxOptions{key: key})
+	}
+	c.key = key
+	return ctx
+}
+
+// WithTTL returns a new Context that carries the TTL for the cache entry.
+func WithTTL(ctx context.Context, ttl time.Duration) context.Context {
+	c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions)
+	if !ok {
+		return context.WithValue(ctx, ctxOptionsKey, &ctxOptions{ttl: ttl})
+	}
+	c.ttl = ttl
+	return ctx
+}
---
+++ driver.go
@@ -0,0 +1,318 @@
+package entcache
+
+import (
+	"context"
+	stdsql "database/sql"
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"time"
+	_ "unsafe"
+
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/dialect/sql"
+	"github.com/mitchellh/hashstructure"
+)
+
+type (
+	// Options wraps the basic configuration cache options.
+	Options struct {
+		// TTL defines the period of time that an Entry
+		// is valid in the cache.
+		TTL time.Duration
+
+		// Cache defines the GetAddDeleter (cache implementation)
+		// for holding the cache entries. If no cache implementation
+		// was provided, an LRU cache with no limit is used.
+		Cache AddGetDeleter
+
+		// Hash defines an optional Hash function for converting
+		// a query and its arguments to a cache key. If no Hash
+		// function was provided, the DefaultHash is used.
+		Hash func(query string, args []interface{}) (Key, error)
+
+		// Logf function. If provided, the Driver will call it with
+		// errors that can not be handled.
+		Log func(...interface{})
+	}
+
+	// Option allows configuring the cache
+	// driver using functional options.
+	Option func(*Options)
+
+	// A Driver is an SQL cached client. Users should use the
+	// constructor below for creating new driver.
+	Driver struct {
+		dialect.Driver
+		*Options
+	}
+)
+
+// NewDriver returns a new Driver an existing driver and optional
+// configuration functions. For example:
+//
+//	entcache.NewDriver(
+//		drv,
+//		entcache.TTL(time.Minute),
+//		entcache.MultiLevel(
+//			NewLRU(256),
+//			NewRedis("localhost:9090"),
+//		)
+//	)
+//
+func NewDriver(drv dialect.Driver, opts ...Option) *Driver {
+	options := &Options{Hash: DefaultHash, Cache: NewLRU(0)}
+	for _, opt := range opts {
+		opt(options)
+	}
+	return &Driver{
+		Driver:  drv,
+		Options: options,
+	}
+}
+
+// TTL configures the period of time that an Entry
+// is valid in the cache.
+func TTL(ttl time.Duration) Option {
+	return func(o *Options) {
+		o.TTL = ttl
+	}
+}
+
+// Hash configures an optional Hash function for
+// converting a query and its arguments to a cache key.
+func Hash(hash func(query string, args []interface{}) (Key, error)) Option {
+	return func(o *Options) {
+		o.Hash = hash
+	}
+}
+
+// Levels configures the Driver to work with the given cache levels.
+// For example, in process LRU cache and a remote Redis cache.
+func Levels(levels ...AddGetDeleter) Option {
+	return func(o *Options) {
+		if len(levels) == 1 {
+			o.Cache = levels[0]
+		} else {
+			o.Cache = &multiLevel{levels: levels}
+		}
+	}
+}
+
+// ContextLevel configures the driver to work with context/request level cache.
+// Users that use this option, should wraps the *http.Request context with the
+// cache value as follows:
+//
+//	ctx = entcache.NewContext(ctx)
+//
+//	ctx = entcache.NewContext(ctx, entcache.NewLRU(128))
+//
+func ContextLevel() Option {
+	return func(o *Options) {
+		o.Cache = &contextLevel{}
+	}
+}
+
+// Query implements the Querier interface for the driver. It falls back to the
+// underlying wrapped driver in case of caching error.
+//
+// Note that, the driver does not synchronize identical queries that are executed
+// concurrently. Hence, if 2 identical queries are executed at the ~same time, and
+// there is no cache entry for them, the driver will execute both of them and the
+// last successful one will be stored in the cache.
+func (d *Driver) Query(ctx context.Context, query string, args, v interface{}) error {
+	vr, ok := v.(*sql.Rows)
+	if !ok {
+		return fmt.Errorf("entcache: invalid type %T. expect *sql.Rows", v)
+	}
+	argv, ok := args.([]interface{})
+	if !ok {
+		return fmt.Errorf("entcache: invalid type %T. expect []interface{} for args", args)
+	}
+	opts, err := d.optionsFromContext(ctx, query, argv)
+	if err != nil {
+		return d.Driver.Query(ctx, query, args, v)
+	}
+	switch e, err := d.Cache.Get(ctx, opts.key); {
+	case err == nil:
+		vr.ColumnScanner = &repeater{columns: e.Columns, values: e.Values}
+	case err == ErrNotFound:
+		if err := d.Driver.Query(ctx, query, args, vr); err != nil {
+			return err
+		}
+		vr.ColumnScanner = &recorder{
+			ColumnScanner: vr.ColumnScanner,
+			onClose: func(columns []string, values [][]driver.Value) {
+				err := d.Cache.Add(ctx, opts.key, &Entry{Columns: columns, Values: values}, opts.ttl)
+				if err != nil && d.Log != nil {
+					d.Log(fmt.Sprintf("entcache: failed storing entry %v in cache: %v", opts.key, err))
+				}
+			},
+		}
+	default:
+		return d.Driver.Query(ctx, query, args, v)
+	}
+	return nil
+}
+
+// errSkip tells the driver to skip cache layer.
+var errSkip = errors.New("entcache: skip cache")
+
+// optionsFromContext returns the injected options from the context, or its default value.
+func (d *Driver) optionsFromContext(ctx context.Context, query string, args []interface{}) (ctxOptions, error) {
+	var opts ctxOptions
+	if c, ok := ctx.Value(ctxOptionsKey).(*ctxOptions); ok {
+		opts = *c
+	}
+	if opts.key == nil {
+		key, err := d.Hash(query, args)
+		if err != nil {
+			return opts, errSkip
+		}
+		opts.key = key
+	}
+	if opts.ttl == 0 {
+		opts.ttl = d.TTL
+	}
+	if opts.evict {
+		if err := d.Cache.Del(ctx, opts.key); err != nil {
+			return opts, err
+		}
+	}
+	if opts.skip {
+		return opts, errSkip
+	}
+	return opts, nil
+}
+
+// DefaultHash provides the default implementation for converting
+// a query and its argument to a cache key.
+func DefaultHash(query string, args []interface{}) (Key, error) {
+	key, err := hashstructure.Hash(struct {
+		Q string
+		A []interface{}
+	}{
+		Q: query,
+		A: args,
+	}, nil)
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+// rawCopy copies the driver values by implementing
+// the sql.Scanner interface.
+type rawCopy struct {
+	values []driver.Value
+}
+
+func (c *rawCopy) Scan(src interface{}) error {
+	c.values[0] = src
+	c.values = c.values[1:]
+	return nil
+}
+
+// recorder represents an sql.Rows recorder that implements
+// the entgo.io/ent/dialect/sql.ColumnScanner interface.
+type recorder struct {
+	sql.ColumnScanner
+	values  [][]driver.Value
+	columns []string
+	done    bool
+	onClose func([]string, [][]driver.Value)
+}
+
+// Next wraps the underlying Next method
+func (r *recorder) Next() bool {
+	hasNext := r.ColumnScanner.Next()
+	r.done = !hasNext
+	return hasNext
+}
+
+// Scan copies database values for future use (by the repeater)
+// and assign them to the given destinations using the standard
+// database/sql.convertAssign function.
+func (r *recorder) Scan(dest ...interface{}) error {
+	values := make([]driver.Value, len(dest))
+	args := make([]interface{}, len(dest))
+	c := &rawCopy{values: values}
+	for i := range args {
+		args[i] = c
+	}
+	if err := r.ColumnScanner.Scan(args...); err != nil {
+		return err
+	}
+	for i := range values {
+		if err := convertAssign(dest[i], values[i]); err != nil {
+			return err
+		}
+	}
+	r.values = append(r.values, values)
+	return nil
+}
+
+// Columns wraps the underlying Column method and stores it in the recorder state.
+// The repeater.Columns cannot be called if the recorder method was not called before.
+// That means, raw scanning should be identical for identical queries.
+func (r *recorder) Columns() ([]string, error) {
+	columns, err := r.ColumnScanner.Columns()
+	if err != nil {
+		return nil, err
+	}
+	r.columns = columns
+	return columns, nil
+}
+
+func (r *recorder) Close() error {
+	if err := r.ColumnScanner.Close(); err != nil {
+		return err
+	}
+	// If we did not encounter any error during iteration,
+	// and we scanned all rows, we store it on cache.
+	if err := r.ColumnScanner.Err(); err == nil || r.done {
+		r.onClose(r.columns, r.values)
+	}
+	return nil
+}
+
+// repeater repeats columns scanning from cache history.
+type repeater struct {
+	columns []string
+	values  [][]driver.Value
+}
+
+func (*repeater) Close() error {
+	return nil
+}
+func (*repeater) ColumnTypes() ([]*stdsql.ColumnType, error) {
+	return nil, fmt.Errorf("entcache.ColumnTypes is not supported")
+}
+func (r *repeater) Columns() ([]string, error) {
+	return r.columns, nil
+}
+func (*repeater) Err() error {
+	return nil
+}
+func (r *repeater) Next() bool {
+	return len(r.values) > 0
+}
+func (r *repeater) NextResultSet() bool {
+	return len(r.values) > 0
+}
+
+func (r *repeater) Scan(dest ...interface{}) error {
+	if !r.Next() {
+		return stdsql.ErrNoRows
+	}
+	for i, src := range r.values[0] {
+		if err := convertAssign(dest[i], src); err != nil {
+			return err
+		}
+	}
+	r.values = r.values[1:]
+	return nil
+}
+
+//go:linkname convertAssign database/sql.convertAssign
+func convertAssign(dest, src interface{}) error
